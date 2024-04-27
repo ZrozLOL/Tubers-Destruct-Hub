82c01b40cfb24bb76b41363a167b4ca6d257bc4f
ZrozLOL <168303631+ZrozLOL@users.noreply.github.com>
2024-04-27 23:13:54 +0300

    walkspeed.lua

--- walkspeed.lua
+++ walkspeed.lua
@@ -2,7 +2,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 -- Создать окно UI
-local Window = Library.CreateLib("Name", "RJTheme3")
+local Window = Library.CreateLib("WalkSpeed Hack", "RJTheme3")
 
 -- Секция
 local Tab = Window:NewTab("Main")
